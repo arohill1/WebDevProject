0ee7636f638f141e21623bbc439ea3ad796977cf
Anjali <arohill1@binghamton.edu>
2023-03-06 14:58:46 -0500

    a simple landing page of bank's website

---
+++ app/js/script.js
@@ -0,0 +1,27 @@
+const btnhamburger = document.querySelector("#btnhamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnhamburger.addEventListener("click", function () {
+  console.log("Click Hamburger");
+  //adding a class
+  if (header.classList.contains("open")) {
+    //Close hamburger menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    // open hamburger menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach(function (element) {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
+  }
+});
